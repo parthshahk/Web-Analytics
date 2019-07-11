49ed9192e97c2cd447a6a093552aa2322cced620
Parth Shah <parthshahk@gmail.com>
2019-07-11 19:29:20 +0530

    User Auth

--- server.js
+++ server.js
@@ -2,11 +2,28 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const expressSession = require('express-session');
+
+// Models
+const User = require('./models/User');
 
 // Initialize App
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSession({
+    secret: 'webanalyticspass',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Initialize Database
+mongoose.connect('mongodb://localhost/wanalytics', {useNewUrlParser: true});
+let db = mongoose.connection;
+db.once('open', () => console.log("Database Connected"));
+db.on('error', err => console.log(err));
 
 // Static Folders
 app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
@@ -14,26 +31,62 @@ app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
 
 // Homepage
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    if(req.session.auth == true){
+        res.redirect("/dashboard");
+    }else{
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    }
 });
 
 // Signup
 app.post("/signup", (req, res) => {
+    User.findOne({email: req.body.email}, (err, existence) => {
+        if(!existence){
+            
+            let user = new User();
+            user.name = req.body.name;
+            user.email = req.body.email;
 
-    //check if user exist
-    //add user 
-    //redirect to /login
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(req.body.password, salt);
 
-    console.log(req.body.name);
-    res.end();
+            user.password = hash;
+            user.save(err => {
+                if(!err){
+                    res.redirect(307, '/login');
+                }else{
+                    console.log(err);
+                }
+            })
+        }else{
+            res.redirect('/#userExists');
+        }
+    });
 });
 
-//login
+// Login
 app.post("/login", (req, res) => {
-    //check if user exist
-    //check password
-    //login
-    //redirect to dashboard
+
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(user){
+            if(bcrypt.compareSync(req.body.password, user.password)){
+                req.session.auth = true;
+                res.redirect("/dashboard");
+            }else{
+                res.redirect('/#incorrectPassword');
+            }
+        }else{
+            res.redirect('/#userDoesNotExist');
+        }
+    })
+});
+
+// Logout
+app.get("/logout", (req, res) => {
+    if(req.session.auth = true){
+        req.session.auth = false;
+    }
+    res.redirect("/#logout");
 });
 
 // Initialize Server
