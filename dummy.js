6cc26e60bc2a0290ae627767f906f64b430cb93c
Parth Shah <parthshahk@gmail.com>
2019-08-28 09:32:37 +0530

    dummy

---
+++ dummy.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const User = require('./models/User');
+const Report = require('./models/Report');
+const Event = require('./models/Event');
+
+const app = express();
+
+mongoose.connect(config.local_db, {useNewUrlParser: true});
+let db = mongoose.connection;
+db.once('open', () => console.log("Database Connected"));
+db.on('error', err => console.log(err));
+
+
+function randomDate(start, end) {
+    var date1 = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+    return date1.toISOString().substr(0, date1.toISOString().length -5)
+}
+
+function randomData(){
+
+    var data = ['a', 'b', 'c', 'd', 'e', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'x', 'y', 'z', 'w']
+
+    var purchased = []
+
+    var total = Math.floor((Math.random() * 7)+1)
+    
+    for(var i=0; i<total; i++){
+        purchased.push(data[Math.floor(Math.random() * data.length)]);
+    }
+    
+    var purs = '';
+
+    for(var i=0; i<purchased.length; i++){
+        purs += `${purchased[i]},`
+    }
+
+    return purs.substr(0, purs.length-1)
+}
+
+for(var i=0; i<100; i++){
+    
+
+    Event.updateOne(
+        {
+            eventId: 'CEyCWQW'
+        },
+        {
+            $push: {
+                triggers: {
+                    date: randomDate(new Date(2019, 0, 1), new Date(2019, 12, 1))
+                    // data: randomData()
+                }
+            }
+        }, () => {}
+    )
+}
+
+// Initialize Server
+const PORT = process.env.PORT || config.PORT;
+app.listen(PORT, () => {
+    console.log("Server Running");
+});
